65775f5d2412063d4cea24045682873d1e0e5921
Dmitrii <bankersenator@gmail.com>
2023-03-13 11:06:44 +0000

    sm7125-common: BoardConfigCommon.mk: Use trb clang for building kernel
     - Trb clang 17
     - Reference : VoltageOS/manifest@7f29861
    
     git clone https://gitlab.com/varunhardgamer/trb_clang.git -b 17 prebuilts/clang/host/linux-x86/trb_clang
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -60,7 +60,10 @@ TARGET_KERNEL_SOURCE        := kernel/samsung/sm7125
 TARGET_KERNEL_ARCH          := arm64
 TARGET_KERNEL_HEADER_ARCH   := arm64
 TARGET_LINUX_KERNEL_VERSION := 4.14
-TARGET_KERNEL_CLANG_VERSION := r487747
+TARGET_KERNEL_CLANG_VERSION := trb_clang
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/$(TARGET_KERNEL_CLANG_VERSION)
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(TARGET_KERNEL_CLANG_PATH)/bin/aarch64-linux-gnu-
+TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX := $(TARGET_KERNEL_CLANG_PATH)/bin/arm-linux-gnueabi-
 
 # Kernel flags
 BOARD_KERNEL_CMDLINE += console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=1 androidboot.usbcontroller=a600000.dwc3 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image
